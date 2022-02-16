2f5e2157fb665e62618a52cf6fe5cff3841fa893
Corentin Peyresblanques <corentin.peyresblanques@gmail.com>
2022-02-16 16:13:02 +0100

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'The best Trap House in all NYC',
+  address: "Two steps from Central Park's Lidl",
+  description: "Simply THE place to have fun in the Big Apple. Meet your new flat-mates: Augustin and Michel. They just got their factory closed so they might seem depressed. But we promise, they don't bite. (WC are shared, sorry)",
+  price_per_night: 10,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Almost a brothel',
+  address: "Tal'dorei",
+  description: 'The owner, Scanlan, will be pleased to meet you and to make you discover the pleasures of his mighty love beads.',
+  price_per_night: 86,
+  number_of_guests: 8
+)
+Flat.create!(
+  name: "At your mom's",
+  address: "Still at your mom's",
+  description: "The feeling of being at home with your parents, but you paid for it.",
+  price_per_night: 50,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: "At your dad's",
+  address: "Still at your dad's",
+  description: "Just like your mom's but sadder and cheapper.",
+  price_per_night: 30,
+  number_of_guests: 1
+)
